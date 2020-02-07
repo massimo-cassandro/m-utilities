71b8264e88699e5302acee38c8fe0852f77045a2
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-02-07 15:14:07 +0100

    date in sequenza

---
+++ js-modules/_date_in_sequenza.js
@@ -0,0 +1,41 @@
+export default function () {
+  'use strict';
+  //date in sequenza
+  /*
+    l'attributo `data-min` in un campo data indica l'id del campo il cui valore
+    imposta l'attributo `min` del campo corrente.
+    Funzionamento analogo per l'attributo `data-max`
+
+    Sulla data iniziale → data-max
+    Sulla data finale   → data-min
+  */
+  document.querySelectorAll('input[type="date"][data-min]').forEach(el => {
+    let campo_correlato = document.querySelector('#' + el.dataset.min);
+    campo_correlato.addEventListener('change', () => {
+      el.setAttribute('min', campo_correlato.value);
+    });
+  });
+
+  document.querySelectorAll('input[type="date"][data-max]').forEach(el => {
+    let campo_correlato = document.querySelector('#' + el.dataset.max);
+    campo_correlato.addEventListener('change', () => {
+      el.setAttribute('max', campo_correlato.value);
+    });
+  });
+
+  // $('input[type="date"][data-min]').each(function () {
+  //   var _this = $(this),
+  //     campo_correlato = $('#' + _this.data('min'));
+  //   campo_correlato.change(function () {
+  //     _this.attr('min', $(this).val());
+  //   });
+  // });
+
+  // $('input[type="date"][data-max]').each(function () {
+  //   var _this = $(this),
+  //     campo_correlato = $('#' + _this.data('max'));
+  //   campo_correlato.change(function () {
+  //     _this.attr('max', $(this).val());
+  //   });
+  // });
+}
