29dbf0f80257499cc4e9281edfda67fef77f3c4a
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-03-26 22:48:04 +0100

    fix

--- js-modules/_datetime-bs4-modal-fallback.js
+++ js-modules/_datetime-bs4-modal-fallback.js
@@ -17,6 +17,10 @@
   Se datetime non è supportato, la funzione trasforma il campo in un hidden
   e crea un box per visualizzare la data con un pulsante per la modifica.
 
+  Eventuali attributi min, max e step vengono interpretati e utilizzati nei campi
+  di fallback
+  il valore step è utilizzato solo per il campo time
+
   Se il campo data ora è obbligatorio, è necessario effettuare un controllo ad hoc
   al momento del submit
 
@@ -36,51 +40,86 @@ export  function datetimeModalFallback(datetime_field) {
       hour:'2-digit',
       minute:'2-digit'
     },
-
-    modal = `<div class="modal fade" id="modalDatetimeFallback" tabindex="-1" role="dialog" aria-labelledby="modalDatetimeFallbackTitle" aria-hidden="true">
-      <div class="modal-dialog modal-sm" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <div class="modal-title" id="modalDatetimeFallbackTitle">Imposta data &amp; ora</div>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Chiudi">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div id="modalDatetimeFallbackInfo"></div>
-
-            <div class="form-group">
-              <label for="modal-datetime-fallback-date">Data</label>
-              <input class="form-control" id="modal-datetime-fallback-date" value="" type="date">
-            </div>
-
-            <div class="form-group">
-              <label for="modal-datetime-fallback-time">Ora</label>
-              <input class="form-control" id="modal-datetime-fallback-time" value="" type="time">
-            </div>
-
-          </div>
-          <div class="modal-footer">
-          <button id="modal-datetime-set-date" type="button" class="btn btn-primary">Imposta data/ora</button>
-        </div>
-        </div>
-      </div>
-    </div>`,
+    step_attr = datetime_field.step || null,
+    min_attr = datetime_field.min || null,
+    max_attr = datetime_field.max || null,
 
     fallback_container_id = datetime_field.id + '-dt-fallback-show',
     fallback_trigger_id = datetime_field.id + '-dt-fallback-trigger';
+
+  const modal = `<div class="modal fade" id="modalDatetimeFallback" tabindex="-1" role="dialog" aria-labelledby="modalDatetimeFallbackTitle" aria-hidden="true">
+    <div class="modal-dialog modal-sm" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <div class="modal-title" id="modalDatetimeFallbackTitle">Imposta data &amp; ora</div>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Chiudi">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div id="modalDatetimeFallbackInfo"></div>
+
+          <div class="form-group">
+            <label for="modal-datetime-fallback-date">Data</label>
+            <input class="form-control" id="modal-datetime-fallback-date" value="" type="date">
+          </div>
+
+          <div class="form-group">
+            <label for="modal-datetime-fallback-time">Ora</label>
+            <input class="form-control" id="modal-datetime-fallback-time" value="" type="time">
+          </div>
+
+        </div>
+        <div class="modal-footer">
+        <button id="modal-datetime-set-date" type="button" class="btn btn-primary">Imposta data/ora</button>
+      </div>
+      </div>
+    </div>
+  </div>`;
 
   datetime_field.setAttribute('type', 'hidden');
   datetime_field.required = false;
 
   document.body.insertAdjacentHTML('beforeend', modal);
+
   const modalDatetimeFallback = document.querySelector('#modalDatetimeFallback'),
     date_fallback_field = modalDatetimeFallback.querySelector('#modal-datetime-fallback-date'),
-    time_fallback_field = modalDatetimeFallback.querySelector('#modal-datetime-fallback-time');
+    time_fallback_field = modalDatetimeFallback.querySelector('#modal-datetime-fallback-time'),
+
+    datetime_split = datetime_str => {
+      let [d,t] = datetime_str.split( datetime_str.indexOf('T') !== -1 ? 'T' : ' ' );
+      let [h,m] = t.split(':')
+      if(+h !== 0 || +m !== 0) {
+        t = String('00' + Number(h)).slice(-2) + ':' + String('00' + Number(m)).slice(-2);
+      } else {
+        t = null;
+      }
+
+      return [d,t];
+    };
+
+  if(step_attr) {
+    time_fallback_field.step = step_attr;
+  }
+  if(min_attr) {
+    let [d,t] = datetime_split(min_attr);
+    date_fallback_field.min = d;
+    if(t) {
+      time_fallback_field.min = t;
+    }
+  }
+  if(max_attr) {
+    let [d,t] = datetime_split(max_attr);
+    date_fallback_field.max = d;
+    if(t) {
+      time_fallback_field.max = t;
+    }
+  }
 
   datetime_field.insertAdjacentHTML('beforebegin',
     '<div class="input-group mb-3">'+
-      `<span id="${fallback_container_id}" class="form-control bg-transparent text-white text-truncate text-monospace font-weight-normal">` +
+      `<span id="${fallback_container_id}" ` +
+        'class="form-control bg-transparent text-white text-truncate text-monospace font-weight-normal">' +
       (datetime_field.value?
         new Date(datetime_field.value).toLocaleString('it-IT', localeOptions) :
         '&mdash;'
@@ -111,7 +150,25 @@ export  function datetimeModalFallback(datetime_field) {
 
   modalDatetimeFallback.querySelector('#modal-datetime-set-date').addEventListener('click', () => {
 
-    if(date_fallback_field.value && time_fallback_field.value) {
+    let alert = modalDatetimeFallback.querySelector('.alert');
+    if(alert) {
+      alert.remove();
+    }
+
+    if(!date_fallback_field.value || !time_fallback_field.value) {
+      modalDatetimeFallback.querySelector('#modalDatetimeFallbackInfo')
+        .insertAdjacentHTML('beforebegin',
+          '<div class="alert alert-dismissible alert-danger" role="alert">'+
+          '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Chiudi</span></button>'+
+          'Devi inserire data e ora'+
+          '</div>'
+        );
+
+    } else if( modalDatetimeFallback.querySelectorAll(':invalid').length ) {
+
+      modalDatetimeFallback.querySelector(':invalid').reportValidity();
+
+    } else {
       let nuova_data = date_fallback_field.value + 'T' + time_fallback_field.value;
 
       datetime_field.value = nuova_data;
@@ -121,15 +178,6 @@ export  function datetimeModalFallback(datetime_field) {
 
       $('#modalDatetimeFallback').modal('hide');
 
-    } else {
-
-      modalDatetimeFallback.querySelector('#modalDatetimeFallbackInfo')
-        .insertAdjacentHTML('beforebegin',
-          '<div class="alert alert-dismissible alert-danger" role="alert">'+
-          '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Chiudi</span></button>'+
-          'Devi inserire data e ora'+
-          '</div>'
-        );
     }
   }, false)
 
