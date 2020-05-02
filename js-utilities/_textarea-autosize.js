d2f9b62acbce1336dbbe258089cb45a310bc1222
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-05-02 19:33:24 +0200

    textarea autosize

---
+++ js-utilities/_textarea-autosize.js
@@ -0,0 +1,33 @@
+/*
+  textarea autosize
+
+  si applica agli elementi textarea con classe 'autosize'
+  consigliabile aggiungere una proprietà min-height ai textarea
+
+  per ridimensionare un elemento textarea in seguito all'aggiunta di un testo
+  dinamico, effettuare il trigger dell'evento input:
+
+    target_textarea.dispatchEvent('input')
+
+
+  Credits:
+  https://github.com/javierjulio/textarea-autosize/
+*/
+export default function () {
+  'use strict';
+
+  // select interested textareas
+  let textareas = document.querySelectorAll('textarea.autosize');
+  textareas.forEach(el => {
+    let cstyle = window.getComputedStyle(el, null),
+      v_borders  = parseInt(cstyle.getPropertyValue('border-top-width')) +
+        parseInt(cstyle.getPropertyValue('border-bottom-width')) || 0;
+
+    el.style.height = (el.scrollHeight - v_borders) + 'px';
+
+    el.addEventListener('input', () => {
+      el.style.height = 0;
+      el.style.height = (el.scrollHeight - v_borders) + 'px';
+    });
+  });
+}
