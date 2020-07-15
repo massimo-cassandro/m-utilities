ebc8b59b29f9ac36a3624183f16b964af685d22e
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-07-15 17:06:57 +0200

    number format update

--- js-utilities/_number_format.js
+++ js-utilities/_number_format.js
@@ -4,27 +4,35 @@ export  function number_format (number, decimals) {
   if(decimals === undefined) decimals=0;
   number = Number( number);
 
-  function toLocaleStringSupportsLocales() {
-    var nn = 0;
-    try {
-      nn.toLocaleString('i');
-    } catch (e) {
-      return e.name === 'RangeError';
-    }
-    return false;
-  }
-
-  if(toLocaleStringSupportsLocales()) {
-    number = Number(number.toFixed(decimals));
-    return number.toLocaleString('it-IT', { minimumFractionDigits: decimals });
-
+  if(decimals) {
+    number = Math.round((number + Number.EPSILON) * Math.pow(10, decimals)) / Math.pow(10, decimals);
   } else {
-
-    // http://stackoverflow.com/questions/2254185/regular-expression-for-formatting-numbers-in-javascript
-    number = number.toFixed( decimals );
-    var number_parts = String(number).split('.');
-
-    return number_parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.') + ( decimals ? ',' + number_parts[1] : '');
+    number = Math.round(number);
   }
+  return number.toLocaleString('it-IT', { minimumFractionDigits: decimals });
+
+  // function toLocaleStringSupportsLocales() {
+  //   var nn = 0;
+  //   try {
+  //     nn.toLocaleString('i');
+  //   } catch (e) {
+  //     return e.name === 'RangeError';
+  //   }
+  //   return false;
+  // }
+
+  // if(toLocaleStringSupportsLocales()) {
+  //   // Math.round((delta + Number.EPSILON) * 100) / 100;
+  //   number = Number(number.toFixed(decimals));
+  //   return number.toLocaleString('it-IT', { minimumFractionDigits: decimals });
+
+  // } else {
+
+  //   // http://stackoverflow.com/questions/2254185/regular-expression-for-formatting-numbers-in-javascript
+  //   number = number.toFixed( decimals );
+  //   var number_parts = String(number).split('.');
+
+  //   return number_parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.') + ( decimals ? ',' + number_parts[1] : '');
+  // }
 }
 
