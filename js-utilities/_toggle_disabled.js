ecada9b0ec6599ebe8b3aad8737786c9a461c0c1
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-09-02 13:06:44 +0200

    toggle_disabled: added disabled class to disabled elements

--- js-utilities/_toggle_disabled.js
+++ js-utilities/_toggle_disabled.js
@@ -1,23 +1,24 @@
 /*
-  toggle disabled attribute of given field according to `is_disabled` parameter
+  toggle disabled attribute of given element according to `is_disabled` parameter
   toggle `disabled` class to closest `.form-group` parent
   and `disabled` attribute children buttons
 
   Works both with jquery and vanilla dom objects
 
 */
-export default function (field, is_disabled) {
+export default function (element, is_disabled) {
   'use strict';
 
-  if(window.jQuery !== undefined && field instanceof window.jQuery) {
-    field.prop('disabled', is_disabled)
+  if(window.jQuery !== undefined && element instanceof window.jQuery) {
+    element.prop('disabled', is_disabled).toggleClass('disabled', is_disabled)
       .closest('.form-group').toggleClass('disabled', is_disabled)
       .find('button').prop('disabled', is_disabled);
 
   } else {
-    field.setAttribute('disabled', is_disabled);
-    field.closest('.form-group').classList.toggle('disabled', is_disabled);
-    field.closest('.form-group').querySelectorAll('button').forEach(item => {
+    element.setAttribute('disabled', is_disabled);
+    element.classList.toggle('disabled', is_disabled);
+    element.closest('.form-group').classList.toggle('disabled', is_disabled);
+    element.closest('.form-group').querySelectorAll('button').forEach(item => {
       item.setAttribute('disabled', is_disabled);
     });
   }
