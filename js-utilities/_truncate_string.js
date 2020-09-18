40c2162f1585da7909b31208e6c91ded59b7771e
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-09-18 12:56:50 +0200

    truncate string fix

--- js-utilities/_truncate_string.js
+++ js-utilities/_truncate_string.js
@@ -2,6 +2,8 @@
 export function truncate(str, max, suffix) {
   'use strict';
   suffix = suffix || '…';
-  return str.length < max ? str :
-    `${str.substr(0, str.substr(0, max - suffix.length).lastIndexOf(' '))}${suffix}`;
+  if(str) {
+    return str.length < max ? str :
+      `${str.substr(0, str.substr(0, max - suffix.length).lastIndexOf(' '))}${suffix}`;
+  }
 }
