f62cd85d6f455f900a2eb793ab08dc90f037ad45
Massimo Cassandro <massimo.cassandro@gmail.com>
2020-12-18 17:10:59 +0100

    datatable fix

--- auto-datatables/js/_autoDataTable.js
+++ auto-datatables/js/_autoDataTable.js
@@ -38,8 +38,11 @@ import {number_format} from '../../js-utilities/_number_format';
 
 import Mustache from 'mustache/mustache.mjs';
 import moment from 'moment';
+import it from 'moment/locale/it'
 
 export  function _autoDataTable( $container = '.dt_container', cdt_options = {}, bs4 = true ) {
+
+  moment.locale('it', it)
 
   if(!($container instanceof $)) {
     $container = $($container);
