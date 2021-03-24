157c5046b803c1958ee54325dafa14e866be5bf6
Massimo Cassandro <massimo.cassandro@gmail.com>
2021-03-24 15:08:15 +0100

    upd

--- boilerplate/_PROJ_ROOT_/_REPO_/public/assets/rollup.config.js
+++ boilerplate/_PROJ_ROOT_/_REPO_/public/assets/rollup.config.js
@@ -1,41 +1,37 @@
 import { terser } from 'rollup-plugin-terser';
 import sourcemaps from 'rollup-plugin-sourcemaps';
-import fs from 'fs';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 
 const terserOptions = {
-    compress: {
-      passes: 2
-    }
-  },
-  js_dirs = [
-    'js/public/',
-    'js/backoffice/'
-  ];
+  compress: {
+    passes: 2
+  }
+};
 
-let config = [];
-
-js_dirs.forEach(dir => {
-
-  // https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j
-  fs.readdirSync(dir).filter(f => /\.js$/.test(f)).forEach(file => {
-
-    config.push(
+export default [
+  {
+    input: 'js/javascript-sandbox.js',
+    plugins: [sourcemaps(), resolve()],
+    output: [
       {
-        // preserveEntrySignatures: false,
-        input: `${dir}${file}`,
-        plugins: [sourcemaps(), resolve(), commonjs()],
-        output: [{
-          file: `${dir}dist/${file.replace('.js', '-min.js')}`,
-          format: 'iife',
-          sourcemap: true,
-          plugins: [terser(terserOptions)]
-        }]
+        file: 'js/javascript-sandbox.min.js',
+        format: 'iife',
+        sourcemap: true,
+        plugins: [terser(terserOptions)]
       }
-    );
-
-  });
-});
-
-export default config;
+    ]
+  },
+  {
+    input: 'js/prism.js',
+    plugins: [sourcemaps(), resolve()],
+    output: [
+      {
+        file: 'js/prism.min.js',
+        format: 'iife',
+        name: 'Prism',
+        sourcemap: false,
+        plugins: [terser(terserOptions)]
+      }
+    ]
+  }
+];
