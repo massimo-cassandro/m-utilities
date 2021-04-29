035960edd976fae726e5354366063af0bfb13d6b
Massimo Cassandro <massimo.cassandro@gmail.com>
2021-04-29 15:49:26 +0200

    toggle_disabled enhancement

--- js-utilities/_toggle_disabled.js
+++ js-utilities/_toggle_disabled.js
@@ -3,17 +3,23 @@
   toggle `disabled` class to closest `.form-group` parent
   and `disabled` attribute children buttons
 
+  element may be a .form-group container (a fieldset that contains radios or checkboxes)
+
   Works both with jquery and vanilla dom objects
 
 */
 export default function (element, is_disabled) {
 
   if(window.jQuery !== undefined && element instanceof window.jQuery) {
-    element.prop('disabled', is_disabled).toggleClass('disabled', is_disabled)
-      .closest('.form-group').toggleClass('disabled', is_disabled)
-      .find('button').prop('disabled', is_disabled);
+    element.prop('disabled', is_disabled).toggleClass('disabled', is_disabled);
+
+    if(!element.hasClass('form-group')) {
+      element.closest('.form-group').toggleClass('disabled', is_disabled)
+        .find('button').prop('disabled', is_disabled);
+    }
 
   } else {
+
     if(is_disabled) {
       element.setAttribute('disabled', true);
     } else {
@@ -21,17 +27,19 @@ export default function (element, is_disabled) {
     }
 
     element.classList.toggle('disabled', is_disabled);
-    let form_group = element.closest('.form-group');
-    if(form_group) {
-      form_group.classList.toggle('disabled', is_disabled);
-      form_group.querySelectorAll('button').forEach(item => {
-        if(is_disabled) {
-          item.setAttribute('disabled', is_disabled);
-        } else {
-          item.removeAttribute('disabled');
-        }
-      });
-    }
-  }
 
+    if(!element.classList.contains('form-group')) {
+      let form_group = element.closest('.form-group');
+      if(form_group) {
+        form_group.classList.toggle('disabled', is_disabled);
+        form_group.querySelectorAll('button').forEach(item => {
+          if(is_disabled) {
+            item.setAttribute('disabled', is_disabled);
+          } else {
+            item.removeAttribute('disabled');
+          }
+        });
+      }
+    } // if(element.classList.contains('form-group'))
+  } // if jQuery
 }
