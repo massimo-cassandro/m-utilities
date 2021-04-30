497dd6a9942746a3706c7418fff2f770e3206372
Massimo Cassandro <massimo.cassandro@gmail.com>
2021-04-30 12:07:26 +0200

    fix form field utilties

--- js-utilities/_form_fields_utilities.js
+++ js-utilities/_form_fields_utilities.js
@@ -5,38 +5,69 @@
 // }
 
 export function setFormElementValue(element, value) {
-  let element_type = element.type.toLowerCase(); //getFieldType(element);
 
-  // select →  value == valore dell'option selezionato
-  if (element_type === 'select-one') {
-    let selected = element.querySelector(`option[value="${value}"]`),
-      selected_index = Array.from(element.querySelectorAll('option')).indexOf(selected);
+  let element_type = element.type; //getFieldType(element);
+  if(element_type) {
+    element_type = element.type.toLowerCase();
 
-      element.selectedIndex = selected_index;
+    // fieldset.form-group (serie radio/checkbox) => value è un array dei valori dei campi selezione
+    if(element_type=== 'fieldset' && element.classList.contains('form-group')) {
 
-  // checkbox|radio →  true == selezionato, false == deselezionato
-  } else if (element_type === 'checkbox' || element_type === 'radio') {
-    element.checked = value;
+      value.forEach(val => {
+        // element.querySelector(`input[value="${val}"]`).checked = true;
+        element.querySelector(`input[value="${val}"]`).click();
+      });
 
-  // ['textarea', 'text', 'date', 'email', 'number'].indexOf(element_type) !== -1
+    // select →  value == valore dell'option selezionato
+    } else if (element_type === 'select-one') {
+      let selected = element.querySelector(`option[value="${value}"]`),
+        selected_index = Array.from(element.querySelectorAll('option')).indexOf(selected);
+
+        element.selectedIndex = selected_index;
+
+    // checkbox|radio →  true == selezionato, false == deselezionato
+    } else if (element_type === 'checkbox' || element_type === 'radio') {
+      element.checked = value;
+
+    // ['textarea', 'text', 'date', 'email', 'number'].indexOf(element_type) !== -1
+    } else {
+      element.value = value;
+    }
   } else {
-    element.value = value;
+    console.error('not element type:');
+    console.error(element);
   }
 }
 
 export function getFormElementValue(element) {
-  let element_type = element.type.toLowerCase(); //getFieldType(element);
+  let element_type = element.type; //getFieldType(element);
 
-  // select
-  if (element_type === 'select-one') {
-    return element.options[element.selectedIndex].value;
+  if(element_type) {
+    element_type = element.type.toLowerCase();
 
-  // checkbox|radio →  true == selezionato, false == deselezionato
-  } else if (element_type === 'checkbox' || element_type === 'radio') {
-    return element.checked;
+    // fieldset.form-group (serie radio/checkbox) =>
+    //         restituisce un array con il valore delle opzioni selezionate
+    if(element_type=== 'fieldset' && element.classList.contains('form-group')) {
+      let values = [];
+      element.querySelectorAll('input:checked').forEach(item => {
+        values.push(item.value);
+      });
+      return values;
 
-  // ['textarea', 'text', 'date', 'email', 'number'].indexOf(element_type) !== -1
+    // select
+    } else if (element_type === 'select-one') {
+      return element.options[element.selectedIndex].value;
+
+    // checkbox|radio →  true == selezionato, false == deselezionato
+    } else if (element_type === 'checkbox' || element_type === 'radio') {
+      return element.checked;
+
+    // ['textarea', 'text', 'date', 'email', 'number'].indexOf(element_type) !== -1
+    } else {
+      return element.value;
+    }
   } else {
-    return element.value;
+    console.error('not element type');
+    console.error(element);
   }
 }
